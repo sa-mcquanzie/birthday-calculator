768a2e27e595efeb0846799cd9e46c4efd893401
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-08-11 13:10:17 +0100

    Adding next birthday arithmetic

--- app.rb
+++ app.rb
@@ -7,8 +7,17 @@ end
 
 post '/response' do
   @name = params[:name]
-  birthday = params[:birthday].split("-")
-  @year, @month, @day = birthday[0], birthday[1], birthday[2]
+  b = params[:birthday].split("-").map(&:to_i)  
+  today = Date.today
+  dob = Date.new(b[0], b[1], b[2])
+
+  if Date.today.yday < dob.yday
+    next_birthday = Date.new(today.year, dob.month, dob.day)
+  else
+    next_birthday = Date.new(today.year + 1, dob.month, dob.day)
+  end
+
+  @days_left = (next_birthday - today).numerator
 
   erb :response
 end
