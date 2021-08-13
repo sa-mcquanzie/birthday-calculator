994953077a404804bd984892d265c7c3a1081c63
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-08-13 11:23:23 +0100

    Adding age arithmetic

--- app.rb
+++ app.rb
@@ -7,31 +7,34 @@ get '/' do
 end
 
 post '/response' do
-  @name = params[:name]
-  b = params[:birthday].split("-").map(&:to_i)  
-  today = Date.today
+  b = params[:birthday].split("-").map(&:to_i)
+  cake_number = rand(1..3)
   dob = Date.new(b[0], b[1], b[2])
+  today = Date.today
 
   if Date.today.yday < dob.yday
     birthday = Date.new(today.year, dob.month, dob.day)
+    next_age = birthday.year - dob.year + 1
+
   else
     birthday = Date.new(today.year + 1, dob.month, dob.day)
+    next_age = birthday.year - dob.year
   end
 
   days_left = (birthday - today).numerator
   formatted_date = birthday.strftime("#{birthday.day.ordinalize} of %B, #{birthday.year}")
+  
   @is_birthday = today.month == birthday.month && today.day == birthday.day
 
   if @is_birthday
     @birthday_message = "Happy Birthday"
   else
-    @birthday_message = "Hello #{@name} :)\nYour birthday is in #{days_left} days on the #{formatted_date}"
+    @birthday_message = "Your #{next_age.ordinalize} birthday is in #{days_left} days on the #{formatted_date}"
   end
-
-  cake_number = rand(1..3)
 
   @cake = url("/images/cake#{cake_number}.jpg")
   @fontstyle = @is_birthday ? "birthday" : "normal"
+  @name = params[:name]
 
   erb :response
 end
